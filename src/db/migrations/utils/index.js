439af9824aaca4609d8ac3891240356c52c1cd8a
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2021-04-28 18:12:53 +0900

    수정: undefined map 문제 해결

--- src/db/migrations/utils/index.js
+++ src/db/migrations/utils/index.js
@@ -1,14 +1,15 @@
-module.exports.fetchData = async function fetchData(sql, queryInterface) {
-  const rawDataList = await queryInterface.sequelize.query(sql);
-  return map((rawData) => { return { ...rawData } }, rawDataList[0]);
-}
-
-module.exports.map = function map(fn, iter) {
+function map(fn, iter) {
   const res = [];
   for (let i = 0; i < iter.length; i++) res.push(fn(iter[i], i, iter));
   return res;
 }
+module.exports.map = map;
 
 module.exports.now = function now() {
   return new Date().toISOString().replace(/T/, " ").replace(/\..+/, "");
+}
+
+module.exports.fetchData = async function fetchData(sql, queryInterface) {
+  const rawDataList = await queryInterface.sequelize.query(sql);
+  return map((rawData) => { return { ...rawData } }, rawDataList[0]);
 }
