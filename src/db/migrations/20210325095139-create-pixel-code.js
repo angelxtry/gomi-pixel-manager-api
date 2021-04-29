122cd1efc334d2d5024fc5e4929283600604e9c9
양원석 <nicholas@gomicorp.com>
2021-04-29 16:33:18 +0900

    수정: 마이그레이션 에러 해결

--- src/db/migrations/20210325095139-create-pixel-code.js
+++ src/db/migrations/20210325095139-create-pixel-code.js
@@ -1,4 +1,7 @@
 "use strict";
+
+const faker = require("faker");
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface
@@ -30,8 +33,80 @@ module.exports = {
           references: { model: "Brands", key: "id" },
         });
       });
+
+    await dataMigration(queryInterface, Sequelize);
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable("PixelCodes");
   },
 };
+
+async function dataMigration(queryInterface, Sequelize) {
+  for (let i = 0; i < 10; i++) {
+    await seeding(queryInterface, Sequelize);
+  }
+}
+
+async function seeding(queryInterface, Sequelize) {
+  const dummyBrand = buildDummyBrandData();
+  await queryInterface.bulkInsert("Brands", [dummyBrand], {});
+  const brands = await queryInterface.sequelize.query(
+    `SELECT * FROM Brands ORDER BY id DESC;`
+  );
+  const brand = brands[0][0];
+  console.log(brand);
+
+  const dateTime = now();
+  await queryInterface.bulkInsert("PixelCodes", [
+    {
+      brandId: brand.id,
+      code: faker.random.number(100000000000),
+      shopName: "haravan",
+      createdAt: dateTime,
+      updatedAt: dateTime,
+    },
+    {
+      brandId: brand.id,
+      code: faker.random.number(100000000000),
+      shopName: `${brand.name} soho mall`,
+      createdAt: dateTime,
+      updatedAt: dateTime,
+    },
+  ]);
+
+  const siteUrl = faker.internet.url();
+  await queryInterface.bulkInsert("ProductPages", [
+    buildDummyProductPage(brand.id, siteUrl),
+    buildDummyProductPage(brand.id, siteUrl),
+    buildDummyProductPage(brand.id, siteUrl),
+  ]);
+}
+
+function buildDummyBrandData() {
+  const dateTime = now();
+  return {
+    name: faker.company.companyName(),
+    platformApiId: null,
+    createdAt: dateTime,
+    updatedAt: dateTime,
+  };
+}
+
+function buildDummyProductPage(brandId, siteUrl) {
+  const dateTime = now();
+  const title = faker.commerce.productName();
+  const sourceUrl = [siteUrl, faker.helpers.slugify(title)].join("/");
+
+  return {
+    brandId,
+    title,
+    uniqueNumber: faker.random.number(),
+    sourceUrl,
+    createdAt: dateTime,
+    updatedAt: dateTime,
+  };
+}
+
+function now() {
+  return new Date().toISOString().replace(/T/, " ").replace(/\..+/, "");
+}
