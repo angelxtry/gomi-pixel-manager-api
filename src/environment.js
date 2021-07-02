c111ad78d12f47c34a764c37e299b317acaec255
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2021-07-02 11:02:25 +0900

    기능: 환경파일 구분기능 수정

--- src/environment.js
+++ src/environment.js
@@ -1,30 +1,23 @@
 const dotenv = require('dotenv')
 const path = require('path')
 
-// dotenv.config({ path: path.resolve(__dirname, '../.env') })
-
-let envPath;
-
 // validate the NODE_ENV
+// 현재 실행 환경이 무엇인지 정의합니다.
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
-switch (NODE_ENV) {
-case 'development':
-  envPath = path.resolve(__dirname, '../.env');
-  break;
-case 'staging':
-  envPath = path.resolve(__dirname, '../.env.staging');
-  break;
-case 'production':
-  envPath = path.resolve(__dirname, '../.env.production');
-  break;
-default:
-  envPath = path.resolve(__dirname, '../.env');
-  break;
+// 실행 환경에 맞는 환경변수 파일을 지정합니다.
+const envPathFinder = {
+  development: '../.env',
+  production: '../.env.production',
 };
 
+// 지정한 파일에 따라 경로객체를 구성합니다.
+const envPath = path.resolve(__dirname, envPathFinder[NODE_ENV]);
+
+// 환경변수 파일의 키-값을 process.env 객체의 환경변수에 등록합니다.
 dotenv.config({ path: envPath });
 
+// 앱에서 사용하게 될 환경만 사용하기 좋게 따로 모아 내보냅니다.
 const environment = {
   /* GENERAL */
   NODE_ENV,
